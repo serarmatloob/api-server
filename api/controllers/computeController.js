d034f0a11a43d3d3c0bbb5480028229543759dc5
Serar Matloob <serarmatloob@oakland.edu>
2019-11-12 19:50:59 -0500

    return result as JSON

--- api/controllers/computeController.js
+++ api/controllers/computeController.js
@@ -2,7 +2,7 @@ exports.add = function (req, res) {
     console.log(req.body.num1)
     console.log(req.body.num2)
     var result = req.body.num1 + req.body.num2;
-    res.json(result);
+    res.json({'result': result});
 };
 
 exports.middleware = function (req, res, next) {
